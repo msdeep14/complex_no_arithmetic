821b2df9f2eb41e326d03acdad38ab26c82620eb
Mandeep Singh <mandeepsinghshekhawat95@gmail.com>
2016-02-04 22:33:37 +0530

    mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -18,6 +18,16 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
